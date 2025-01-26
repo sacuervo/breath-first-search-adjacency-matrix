38b861ec844e920af27669a4284ddf0525bd0bd7
Santiago <sacuervo28@gmail.com>
2025-01-26 14:03:53 -0500

    Initial commit

---
+++ adjacency-matrix-graph/src/main/java/me/santiagocuervo/GraphNode.java
@@ -0,0 +1,11 @@
+package me.santiagocuervo;
+
+public class GraphNode {
+    public String name;
+    public int index;
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
