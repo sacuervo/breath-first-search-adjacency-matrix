10de74400de2cefffa5088775d3b3d09460e18e9
sacuervo <cuervo.santiago@gmail.com>
2025-01-26 15:33:20 -0500

    Corrected addUndirectedEdge wrong index in the second path

--- adjacency-matrix-graph/src/main/java/me/santiagocuervo/Graph.java
+++ adjacency-matrix-graph/src/main/java/me/santiagocuervo/Graph.java
@@ -3,11 +3,27 @@ package me.santiagocuervo;
 import java.util.ArrayList;
 
 public class Graph {
-    ArrayList<GraphNode> nodeList;
-    int[][] adjacencyMatrix;
+    ArrayList<GraphNode> nodeList; // Store all nodes
+    int[][] adjacencyMatrix; // Declare adjacency matrix
 
-    public Graph(ArrayList<GraphNode> nodeList) {
-        this.nodeList = nodeList;
-        adjacencyMatrix = new int[nodeList.size()][nodeList.size()];
+    public Graph(ArrayList<GraphNode> nodeList) { // Receive a node list as a constructor argument
+        this.nodeList = nodeList; // Initialize the instance attribute node list
+        adjacencyMatrix = new int[nodeList.size()][nodeList.size()]; // Initialize the adjacency matrix based on the
+                                                                     // number of nodes
+    }
+
+    public void addUndirectedEdge(int i, int j) { // Bidirectional edges mean both edge paths have to be specified with
+                                                  // 1
+        adjacencyMatrix[i][j] = 1; // First path
+        adjacencyMatrix[j][i] = 1; // Second path
+    }
+
+    public void printAdjacencyMatrix() {
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- adjacency-matrix-graph/src/main/java/me/santiagocuervo/Main.java
+++ adjacency-matrix-graph/src/main/java/me/santiagocuervo/Main.java
@@ -1,7 +1,32 @@
 package me.santiagocuervo;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<GraphNode> nodeList = new ArrayList<>(){}; 
+
+        nodeList.add(new GraphNode("A", 0));
+        nodeList.add(new GraphNode("B", 1));
+        nodeList.add(new GraphNode("C", 2));
+        nodeList.add(new GraphNode("D", 3));
+        nodeList.add(new GraphNode("E", 4));
+
+        Graph g = new Graph(nodeList);
+
+        // A Node edges
+        g.addUndirectedEdge(0, 1);
+        g.addUndirectedEdge(0, 2);
+        g.addUndirectedEdge(0, 3);
+
+        // B Node edges
+        g.addUndirectedEdge(1, 3);
+
+        // C Node edges
+        g.addUndirectedEdge(2, 3);
+
+        // D Node edges 
+        g.addUndirectedEdge(3, 4);
+
+        g.printAdjacencyMatrix();
     }
 }
