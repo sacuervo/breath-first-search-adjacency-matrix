f3752aa72fcc4027477612dcb9988e9a2f93b8c0
sacuervo <cuervo.santiago@gmail.com>
2025-01-27 13:37:37 -0500

    Add getNeighbors at Graph

--- adjacency-matrix-graph/src/main/java/me/santiagocuervo/Graph.java
+++ adjacency-matrix-graph/src/main/java/me/santiagocuervo/Graph.java
@@ -3,19 +3,19 @@ package me.santiagocuervo;
 import java.util.ArrayList;
 
 public class Graph {
-    ArrayList<GraphNode> nodeList; // Store all nodes
-    int[][] adjacencyMatrix; // Declare adjacency matrix
+    ArrayList<GraphNode> nodeList;
+    int[][] adjacencyMatrix;
 
-    public Graph(ArrayList<GraphNode> nodeList) { // Receive a node list as a constructor argument
-        this.nodeList = nodeList; // Initialize the instance attribute node list
-        adjacencyMatrix = new int[nodeList.size()][nodeList.size()]; // Initialize the adjacency matrix based on the
-                                                                     // number of nodes
+    public Graph(ArrayList<GraphNode> nodeList) {
+        this.nodeList = nodeList;
+        adjacencyMatrix = new int[nodeList.size()][nodeList.size()];
+
     }
 
-    public void addUndirectedEdge(int i, int j) { // Bidirectional edges mean both edge paths have to be specified with
-                                                  // 1
-        adjacencyMatrix[i][j] = 1; // First path
-        adjacencyMatrix[j][i] = 1; // Second path
+    public void addUndirectedEdge(int i, int j) {
+
+        adjacencyMatrix[i][j] = 1;
+        adjacencyMatrix[j][i] = 1;
     }
 
     public void printAdjacencyMatrix() {
@@ -25,5 +25,19 @@ public class Graph {
             }
             System.out.println();
         }
+    }
+
+    // get neighbors
+    public ArrayList<GraphNode> getNeighbors(GraphNode node) {
+        ArrayList<GraphNode> neighbors = new ArrayList<>();
+        int nodeIndex = node.index;
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[nodeIndex][i] == 1) {
+                neighbors.add(nodeList.get(i));
+            }
+        }
+
+        return neighbors;
     }
 }
